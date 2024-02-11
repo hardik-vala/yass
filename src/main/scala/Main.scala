586a5e959494f7bc5b9a15b1f98e44f8b92f623c
Hardik Vala <hardikvala24@gmail.com>
2024-02-11 05:12:59 +0000

    Define SudokuBoard.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,13 @@
-def func(): Int = {
-  42
+case class SudokuBoard(
+  n: Int,
+  board: Array[Array[Int]]
+) {
+  val dim = n * n
+  require(board.length == dim, f"Board must have ${dim} rows")
+
+  board.foreach{ row => 
+    require(row.length == dim, f"Each row must have ${dim} numbers")
+  }
 }
 
 @main def main(): Unit =
--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,9 +1,23 @@
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
-class MySuite extends munit.FunSuite {
-  test("func that succeeds") {
-    val obtained = func()
-    val expected = 42
-    assertEquals(obtained, expected)
+class MainTest extends munit.FunSuite {
+  test("Valid Sudoku board") {
+    SudokuBoard(2, Array(
+      Array.fill(4)(0),
+      Array.fill(4)(0),
+      Array.fill(4)(0),
+      Array.fill(4)(0),
+    ))
+  }
+
+  test("Invalid Sudoku board throws exception") {
+    intercept[IllegalArgumentException] {
+      SudokuBoard(3, Array(
+        Array.fill(4)(0),
+        Array.fill(4)(0),
+        Array.fill(4)(0),
+        Array.fill(4)(0),
+      ))
+    }
   }
 }
