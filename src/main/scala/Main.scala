d9dfee963573d23554c65bf06d5d94a6b5359654
Hardik Vala <hardikvala24@gmail.com>
2024-02-13 04:26:22 +0000

    Add printing to console.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,7 +8,22 @@ case class SudokuBoard(
   board.foreach{ row => 
     require(row.length == dim, f"Each row must have ${dim} numbers")
   }
+
+  def printToConsole(): Unit = {
+    board.foreach { row =>
+      row.foreach(num => print(f"$num%1d ")) 
+      println()
+    }
+  }
 }
 
+class SudokuSolver(board: SudokuBoard) {}
+
 @main def main(): Unit =
-  println("Hello world!")
+  val b = SudokuBoard(2, Array(
+    Array(4, 3, 4, 4),
+    Array(4, 4, 2, 1),
+    Array(1, 4, 4, 3),
+    Array(4, 4, 4, 1),
+  ))
+  b.printToConsole() 
